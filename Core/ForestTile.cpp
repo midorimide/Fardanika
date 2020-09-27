a5a1bd29174b4841ca01da1820ff82cfb3d8e11a
licham <silaievmov@gmail.com>
2020-09-28 00:57:53 +0900

    Added textures to resources

--- Core/ForestTile.cpp
+++ Core/ForestTile.cpp
@@ -5,7 +5,7 @@ static sf::Texture* texture;
 ForestTile::ForestTile() {
 	if (texture == NULL) {
 		texture = new sf::Texture();
-		texture->loadFromFile("C:/Users/silaiev/Pictures/forest.png");
+		texture->loadFromFile("forest.png");
 	}
 	setTexture(texture);
 }
